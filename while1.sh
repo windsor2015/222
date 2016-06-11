9bef492878ec81ee91c9b65c93b95d249a91a443
windsor2015 <wenxiaohui13@cdut.cn>
2016-06-11 16:58:50 +0800

    while

---
+++ while1.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+count=0
+total=0
+while read student score
+do
+count=`expr $count + 1`
+total=`expr $total + $score`
+done
+avg=`expr $total / $count`
+echo "There are" $count "students,the avg score is:" $avg
